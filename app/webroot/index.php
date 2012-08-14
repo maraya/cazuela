2d5dcdb7a360528be4caf676ca7e3d54e9e90311
maraya <marayag@gmail.com>
2012-08-14 12:58:07 -0300

    Error reporting

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -3,10 +3,22 @@
 /** 
  * Application index
  * @author maraya
- * Version: 0.1 (26 July 2012)
+ * 
+ * This file is part of Cazuela.
+ *
+ * Cazuela is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Cazuela is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Cazuela. If not, see <http://www.gnu.org/licenses/>.
  */
-
-error_reporting(E_ALL | E_STRICT);
 
 /**
  * If Cazuela is in a different path, please change the CAZUELA_BASE constant
@@ -24,6 +36,12 @@ if (!defined('CAZUELA_APP_ROOT')) {
 
 require(CAZUELA_APP_ROOT . "/config/Config.php");
 require(CAZUELA_BASE . "/lib/core/Bootstrap.php");	
+
+if (Configure::read('debug') === 1) {
+	error_reporting(E_ALL | E_STRICT);
+} else {
+	error_reporting(0);
+}
 		
 $dispatcher = new Dispatcher();
 $request = new CazuelaRequest();
